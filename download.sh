872ddaa32c052e2904dd543db4750905db99fe87
jxu <7989982+jxu@users.noreply.github.com>
2021-08-18 00:54:08 -0400

    Fix GIF downloading and combine PDF

--- download.sh
+++ download.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 for i in $(seq -f "%03g" $1 $2); do 
     URL="https://projecteuler.net/problem=$i"
+    # chromium print to PDF
     chromium-browser --headless --disable-gpu --print-to-pdf-no-header --print-to-pdf=$i.pdf $URL   
-    curl -s $URL | pup 'a attr{href}'  | grep '\.txt$' | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r curl -O 
-    curl -s $URL | pup 'img attr{src}' | grep '\.gif$' | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r curl -O 
+    # download extra txt and GIF files if available
+    curl -s $URL | pup 'a attr{href}'  | grep '\.txt$' | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r -n1 curl -O 
+    curl -s $URL | pup 'img attr{src}' | grep '\.gif$' | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r -n1 curl -O 
 done
 
+# remove non-animated GIFs
+for i in *.gif; do 
+    [ $(identify "$i" | wc -l) -le 1 ] && rm -v "$i"
+done
+
+# combine all PDFs in order
+pdftk *.pdf cat output problems.pdf
