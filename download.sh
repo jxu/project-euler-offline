a7446113ec3eb97716915759fa1c4f32db54d42e
jxu <7989982+jxu@users.noreply.github.com>
2022-08-04 02:27:44 -0400

    Refactor pup/curl line into its own function

--- download.sh
+++ download.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+tmphtml=tmp.html
+
+# takes html as stdin, download extra txt and gif files if available 
+# prints links to stderr. credit: tripleee on codereview
+pupcurl () {
+    pup "$1" | grep "$2" |
+    tee /dev/stderr |
+    sed 's%^%https://projecteuler.net/%' |
+    xargs -r -n1 curl -O
+}
+
 for i in $(seq -f "%03g" $1 $2); do 
     URL="https://projecteuler.net/problem=$i"
     # chromium print to PDF, wait for rendering https://stackoverflow.com/a/49789027
@@ -7,9 +19,9 @@ for i in $(seq -f "%03g" $1 $2); do
     # Distill PDFs to workaround Ghostscript skipped character problem https://stackoverflow.com/questions/12806911
     gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -o ${i}_gs.pdf $i.pdf
 
-    # download extra txt and GIF files if available, printing links to shell
-    curl -s $URL | pup 'a attr{href}'  | grep '\.txt$' | tee /dev/tty | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r -n1 curl -O 
-    curl -s $URL | pup 'img attr{src}' | grep '\.gif$' | tee /dev/tty | sed 's/^/https:\/\/projecteuler.net\//' | xargs -r -n1 curl -O 
+    curl "$URL" > "$tmphtml"
+    pupcurl 'a attr{href}' '\.txt$' < "$tmphtml"
+    pupcurl 'img attr{src}' '\.gif$' < "$tmphtml"
 done
 
 # remove non-animated GIFs
