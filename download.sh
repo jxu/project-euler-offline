34bbae22161acc83b448b5987f6cf0681778949f
jxu <7989982+jxu@users.noreply.github.com>
2022-08-04 20:37:21 -0400

    Refactor variable naming and quoting from shellcheck

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tmphtml=tmp.html
+tmp_html=tmp.html
+problem_url="https://projecteuler.net/problem=$i"
 
 # takes html as stdin, download extra txt and gif files if available 
 # prints links to stderr. credit: tripleee on codereview
@@ -11,26 +12,33 @@ pupcurl () {
     xargs -r -n1 curl -O
 }
 
-for i in $(seq -f "%03g" $1 $2); do 
-    URL="https://projecteuler.net/problem=$i"
-    # chromium print to PDF, wait for rendering https://stackoverflow.com/a/49789027
-    chromium-browser --headless --disable-gpu --run-all-compositor-stages-before-draw --virtual-time-budget=10000 --print-to-pdf-no-header --print-to-pdf=$i.pdf $URL
+for i in $(seq -f "%03g" "$1" "$2"); do 
+    # chromium print to PDF, wait for rendering 
+    # https://stackoverflow.com/a/49789027
+    chromium-browser --headless --disable-gpu \
+        --run-all-compositor-stages-before-draw \
+        --virtual-time-budget=10000 \
+        --print-to-pdf-no-header \
+        --print-to-pdf="$i.pdf" "$problem_url"
 
-    # Distill PDFs to workaround Ghostscript skipped character problem https://stackoverflow.com/questions/12806911
-    gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -o ${i}_gs.pdf $i.pdf
+    # Distill PDFs to workaround Ghostscript skipped character problem 
+    # https://stackoverflow.com/questions/12806911
+    gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -o "${i}_gs.pdf" "$i.pdf"
 
-    curl "$URL" > "$tmphtml"
-    pupcurl 'a attr{href}' '\.txt$' < "$tmphtml"
-    pupcurl 'img attr{src}' '\.gif$' < "$tmphtml"
+    curl -s "$problem_url" > "$tmp_html"
+    pupcurl 'a attr{href}' '\.txt$' < "$tmp_html"
+    pupcurl 'img attr{src}' '\.gif$' < "$tmp_html"
 done
 
 # remove non-animated GIFs
 for i in *.gif; do 
-    [ $(identify "$i" | wc -l) -le 1 ] && rm -v "$i"
+    [ "$(identify "$i" | wc -l)" -le 1 ] && rm -v "$i"
 done
 
 # combine all PDFs using gs
-gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOutputFile=problems.pdf *_gs.pdf
+gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite \
+    -dPDFSETTINGS=/ebook \
+    -sOutputFile=problems.pdf ./*_gs.pdf
 # create final zip
-zip problems.zip problems.pdf *.txt *.gif
+zip problems.zip problems.pdf ./*.txt ./*.gif
 
