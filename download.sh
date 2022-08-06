e403dfd4b1c01cbb5a50b639fc7e4720763ff7bb
jxu <7989982+jxu@users.noreply.github.com>
2022-08-06 03:25:07 -0400

    Retry chromium PDF download (#3), comment code

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# takes html as stdin, download extra txt and gif files if available 
+# filters by pup tags and file extension, then curl found files
 # prints files found. refactor credit: tripleee on codereview
 pupcurl () {
     pup "$1" | grep "$2" |
@@ -9,22 +9,31 @@ pupcurl () {
         curl -sS -w "Downloading extra %{filename_effective}\n" -O
 }
 
+# loop through problem number given as script args
 for i in $(seq -f "%03g" "$1" "$2"); do 
     problem_url="https://projecteuler.net/problem=$i"
     tmp_html=tmp.html
 
-    # chromium print to PDF, wait for rendering 
-    # https://stackoverflow.com/a/49789027
-    chromium-browser --headless --disable-gpu \
-        --run-all-compositor-stages-before-draw \
-        --virtual-time-budget=10000 \
-        --print-to-pdf-no-header \
-        --print-to-pdf="$i.pdf" "$problem_url"
+    # workaround for chromium sometimes failing (issue #3)
+    # do-while loop until PDF is non-blank
+    while true; do 
+        # chromium print to PDF, wait for rendering 
+        # https://stackoverflow.com/a/49789027
+        chromium-browser --headless --disable-gpu \
+            --run-all-compositor-stages-before-draw \
+            --virtual-time-budget=10000 \
+            --print-to-pdf-no-header \
+            --print-to-pdf="$i.pdf" "$problem_url"
+        
+        # use 10 KB blank PDF threshold
+        [ "$(stat -c "%s" "$i.pdf")" -gt 10000 ] && break
+    done
 
     # Distill PDFs to workaround Ghostscript skipped character problem 
     # https://stackoverflow.com/questions/12806911
     gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -o "${i}_gs.pdf" "$i.pdf"
 
+    # takes html as stdin, download extra txt and gif files if available 
     curl -sS "$problem_url" > "$tmp_html"
     pupcurl 'a attr{href}' '\.txt$' < "$tmp_html"
     pupcurl 'img attr{src}' '\.gif$' < "$tmp_html"
