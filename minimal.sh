f33c84d31b6323b40801033e8008353fd32cf9fc
jxu <7989982+jxu@users.noreply.github.com>
2024-08-03 21:43:28 -0400

    Convert and unescape minimal HTML to Markdown

--- minimal.sh
+++ minimal.sh
@@ -1,7 +1,14 @@
 rm -f minimal.html
+
+# concat minimal HTML fragments
 for i in {1..903}; do
     echo $i
     echo "<h1>Problem $i</h1>" >> minimal.html
     curl -sS https://projecteuler.net/minimal=$i >> minimal.html
-    echo >> minimal.html
+    echo >> minimal.html  # spacing for human-readability
 done
+
+# convert HTML fragment to Markdown
+# replace pandoc's escaping of \ $ , ^ EOL with non-escaped character
+pandoc --to markdown minimal.html | \
+sed -E 's,\\([\\\$,\^]),\1,g; s,\\$,,g' > minimal.md
